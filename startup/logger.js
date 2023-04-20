cdcf3f44c4620de22a13c817163b60225a6cbc1a
mohammad-jahansoz <big_mamadi@yahoo.com>
2023-04-20 16:13:41 +0330

    colorize error in console for debug better

--- startup/logger.js
+++ startup/logger.js
@@ -1,4 +1,19 @@
 const winston = require("winston");
+let alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.label({
+    label: "[LOGGER]",
+  }),
+  winston.format.timestamp({
+    format: "YY-MM-DD HH:mm:ss",
+  }),
+  winston.format.printf(
+    (info) =>
+      ` ${info.label}  ${info.timestamp}  ${info.level} : ${info.message}`
+  )
+);
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
@@ -7,14 +22,24 @@ const logger = winston.createLogger({
       db: "mongodb://127.0.0.1:27017/shop-db",
       level: "info",
     }),
+    new winston.transports.Console({
+      level: "info",
+      format: alignColorsAndTime,
+    }),
   ],
   exceptionHandlers: [
+    new winston.transports.Console({
+      format: alignColorsAndTime,
+    }),
     new winston.transports.File({
       filename: "uncaughtExceptions.log",
       format: winston.format.json(),
     }),
   ],
   rejectionHandlers: [
+    new winston.transports.Console({
+      format: alignColorsAndTime,
+    }),
     new winston.transports.File({
       filename: "uncaughtExceptions.log",
       format: winston.format.json(),
